6cab0755a60e70c6f475ea0ba1608c600395d111
adityakmr7 <adityakmr9672@gmail.com>
2021-01-24 22:45:23 +0530

    camera added

--- screens/CameraScreen/CameraScreen.tsx
+++ screens/CameraScreen/CameraScreen.tsx
@@ -1,7 +1,8 @@
-import React, { useLayoutEffect } from "react";
-import { Text, View } from "react-native";
+import React, { useLayoutEffect, useState, useEffect } from "react";
+import { Text, View, StyleSheet } from "react-native";
 import { combineTabWithStackProps } from "../../container/Main";
-
+import { Camera } from "expo-camera";
+import { TouchableOpacity } from "react-native-gesture-handler";
 interface CameraScreenProps {
   navigation: combineTabWithStackProps<"camera">;
 }
@@ -9,13 +10,71 @@ const CameraScreen = ({ navigation }: CameraScreenProps) => {
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
+      headerStyle: {
+        display: "none",
+      },
     });
   }, []);
+
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
-    <View>
-      <Text>Camera Screen</Text>
+    <View style={styles.container}>
+      <Camera style={styles.camera} type={type}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}
+          >
+            <Text style={styles.text}> Flip </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: "transparent",
+    flexDirection: "row",
+    margin: 20,
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    color: "white",
+  },
+});
 export default CameraScreen;
